eb1907d3b258ae03aefeda0f5f3ae60fd8a5f427
Egill Anton Hlöðversson <egillanton@live.com>
2018-11-01 21:54:10 +0000

    Ngram demo implemented and Twitter Code Cleaned

--- Twitter.py
+++ Twitter.py
@@ -1,4 +1,5 @@
 from TwitterAPI import TwitterAPI, TwitterPager
+import json
 
 
 class Twitter:
@@ -6,12 +7,20 @@ class Twitter:
     __consumer_secret = 'rKF7W6gxbt9ArjIrrCRvDwxUBQrRROAvRl5hDHm7cAnOpOcWo6'
     __access_token_key = '1051128664631517185-FQhAHJo6WixffqbgIYjXUzYM6YFwO9'
     __access_token_secret = 'hUE0TTAdYclgbhPcUxwpPINx326VzCgaqmQf8PMk0s3Ys'
+    __my_bot_id = '1051128664631517185'
 
     def __init__(self):
-        self.api = TwitterAPI(self.__consumer_key,
-                              self.__consumer_secret,
-                              self.__access_token_key,
-                              self.__access_token_secret)
+        print('Establish connection with Twitter API...')
+
+        try:
+            self.api = TwitterAPI(self.__consumer_key,
+                                  self.__consumer_secret,
+                                  self.__access_token_key,
+                                  self.__access_token_secret)
+        except Exception:
+            print('Connection failed\n')
+
+        print('Connection Established\n')
 
     def get_tweets(self, q, count=5):
         tweets = []
@@ -24,3 +33,16 @@ class Twitter:
                 'SUSPEND, RATE LIMIT EXCEEDED: %s\n' % item['message']
                 break
         return tweets
+
+    def tweet(self, tweet_message):
+        r = self.api.request('statuses/update', {'status': tweet_message})
+        print(f'Tweeted:\n{tweet_message}\nwith Status Code: {r.status_code}')
+        data = json.loads(r.text)
+        return data
+
+    def reply_message_to(self, tweet_message, in_reply_to_status_id):
+        r = self.api.request('statuses/update', {'status': tweet_message, 'in_reply_to_status_id': in_reply_to_status_id})
+        print(f'\nTweeted:\n{tweet_message}\nwith Status Code: {r.status_code}')
+
+    def get_user_stream(self):
+        return self.api.request('statuses/filter', {'follow': self.my_bot_id})
--- server.py
+++ server.py
@@ -1,131 +1,88 @@
-import json
-import re
-import string
-
-import nltk
-from nltk.corpus import stopwords
-
+import os
+import sys
+import glob
+import copy
+from tqdm import trange
+from nltk import ConditionalFreqDist
 from Twitter import Twitter
 
 
-def shutdown_server():
-    print('Shutting Down NLTK Twitter Bot')
-    exit()
+def get_corpus_from_file(path):
+    file_names = glob.glob(os.path.join(path, '*.txt'))
+    corpus = []
+    t = trange(len(file_names), desc='Creating Corpus')
+    for i in t:
+        fin = open(file_names[i], 'r', encoding='utf-8')
+        tokens = []
+        punctuations = [".", ",", ";", ":", "?", "!"]
+        for line in fin:
+            if line.strip() and line.split()[0].strip() not in punctuations:
+                tokens.append(line.split()[0].strip())
+            else:
+                corpus.append(copy.deepcopy(tokens))
+                tokens = []
+        sys.stdout.flush()
+    return corpus
 
 
-def connect_to_twitter():
-    print('Establish connection with Twitter API...')
-    try:
-        api = Twitter.get_api()
-    except Exception:
-        print('Connection failed\n')
-        shutdown_server()
-    print('Connection Established\n')
-    return api
+def get_ngram(corpus, n):
+    ngram = []
+    t = trange(len(corpus), desc=f'Creating {n}-gram')
+    for i in t:
+        sequences = [corpus[i][j:] for j in range(n)]
+        ngram.extend(list(zip(*sequences)))
+    sys.stdout.flush()
+    return ngram
 
 
-def gather_data(api):
-    # TODO: DO we want to gather text from Twitter, or from some ready corpus or both?
-    print('Gathering Data...')
-    sents = []
-
-    # TODO: Gather better Querie Words
-    search_queries = ["Píratar",
-                      "Framsókn",
-                      "Feminismi",
-                      "@tmashelgs",
-                      "#npc"]
-
-    for q in search_queries:
-        print(f'\nGetting Data for: {q}')
-        r = api.request('search/tweets', {'q': q})
-        for item in r:
-            sents.append(item['text'])
-            print(f"{item['id']}: {item['text']}")
-
-    return sents
+def get_conditional_freq_dist(ngram):
+    t = trange(len(ngram), desc=f'Creating Conditional frequency distributions for {len(ngram[0])}-gram')
+    condition_pairs = []
+    for i in t:
+        words = ngram[i]
+        condition_pairs.append((tuple(words[:-1]), words[-1]))
+    return ConditionalFreqDist(condition_pairs)
 
 
-def clean_tweet(a_str):
-    if a_str == '':
-        return a_str
-
-    a_str = re.sub(r"@[A-z]+\w", '', a_str)
-    a_str = re.sub(r"\bhttps:.*\b", '', a_str)
-    a_str = re.sub(r"\bRT\b", '', a_str)
-    a_str = ''.join([char for char in a_str if char not in string.punctuation])
-    a_str = a_str.strip()
+def generate_sent(cdf, words, max_words):
+    first_word = words[0]
+    question_starters = ['er', 'hver', 'hverjir', 'hverjar', 'hvert', 'hvað', 'hvern', 'hverja', 'hverjum', 'hverri',
+                         'hverju', 'hvers', 'hverra', 'hverrar']
+    a_str = ''
+    a_str += first_word.title() + ' '
+    a_str += ' '.join([word for word in words[1:-1]])
+    n = len(cdf.conditions()[0])
+    for i in range(max_words):
+        a_str += words[-1]
+        try:
+            words = words[-(n-1):] + [cdf[tuple(words[-n:])].max()]
+        except ValueError:
+            break
+        if not i == max_words - 1:
+            a_str += ' '
+    if first_word in question_starters:
+        a_str += '?"'
+    else:
+        a_str += '."'
     return a_str
 
 
-
-def model(api, a_input_str):
-    a_input_str = clean_tweet(a_input_str)
-    a_input_str_tokens = a_input_str.split()
-
-    # TODO Remove Stop Words
-    tokens = [token for token in a_input_str_tokens if token not in stopwords.words('english')]
-    tagged_tokens = nltk.pos_tag(tokens)
-
-    # TODO Get Nouns
-    noun_set = ["NN", "NNS"]
-    NN_words = [word for (word, tag) in tagged_tokens if tag in noun_set]
-
-    # TODO Generate responses for given nouns
-    sents = []
-    for q in NN_words:
-        print(f'Getting Data for: {q}')
-        r = api.request('search/tweets', {'q': q,
-                                          'lang': 'eng',
-                                          'include_entities': 'false',
-                                          'result_type': 'recent',
-                                          '-filter': 'retweets',
-                                          'count': '10'})
-        for item in r:
-            full_r = api.request('statuses/show/:' + str(item['id']))
-
-            if full_r.status_code == 200:
-                tweet = full_r.json()
-                text = tweet['text'] if 'text' in tweet else ''
-                text = clean_tweet(text)
-                if (q,text) not in sents:
-                    sents.append((q, text))
-        print('Waiting between Requests')
-
-    # TODO return The Most relevant response
-    if len(sents) > 0:
-        a_output_str = sents[0][1]
-    else:
-        a_output_str = ''
-    return clean_tweet(a_output_str)
-
-
-def tweet(api, tweet_message):
-    r = api.request('statuses/update', {'status': tweet_message})
-    print(f'Tweeted:\n{tweet_message}\nwith Status Code: {r.status_code}')
-    data = json.loads(r.text)
-    return data
-
-
-def reply_to(api, tweet_message, in_reply_to_status_id):
-    r = api.request('statuses/update', {'status': tweet_message, 'in_reply_to_status_id': in_reply_to_status_id})
-    print(f'\nTweeted:\n{tweet_message}\nwith Status Code: {r.status_code}')
-    # start_time = time.time()
-    # if time.time() - start_time > time.time(20):
-
-
-
 def main():
-    print('Starting Up NLTK Twitter Bot')
+    path = './data/IFD1_SETS'
+    corpus = get_corpus_from_file(path)
+    ngram = get_ngram(corpus, 3)
+    cdf = get_conditional_freq_dist(ngram)
+
+    print('Testing out CDF')
+    words = ['er', 'þetta']
+    sent = generate_sent(cdf, words, 15)
+    print(f'Generated Text from the words {words}:\n{sent}')
+
+    print('Starting Up Twitter Bot')
     twitter = Twitter()
 
-    my_bot_id = '1051128664631517185'
-
     # Establishing user stream for my bot
-    r = twitter.api.request('statuses/filter', {'follow': my_bot_id})
-
-    # For every activity regarding my bot
-    for item in r:
+    for item in twitter.get_user_stream():
         if 'in_reply_to_user_id_str' in item and item['in_reply_to_user_id_str'] == my_bot_id:
             # Receive data about the sender and the tweet
             sender_tweet_id = item['id'] if 'id' in item else ''
@@ -134,20 +91,14 @@ def main():
             sender_tweet_text = item['text'] if 'text' in item else ''
 
             # Analyse Comment
-            model_output = model(twitter.api, sender_tweet_text)
+            # model_output = model(twitter.api, sender_tweet_text)
+            model_output = ""
+
             # Generate response
             if sender_user_name != '' and model_output != '':
                 my_bot_response = f'@{sender_user_name} {model_output}'
-                reply_to(twitter.api, my_bot_response, sender_tweet_id)
+                twitter.reply_message_to(my_bot_response, sender_tweet_id)
 
     print('Shutting Down NLTK Twitter Bot')
 
-
 main()
-
-# twitter = Twitter()
-# sents = twitter.get_tweets('cars')
-# print(f'len(sents): {len(sents)}')
-# out = model(api, '@MLVbot: car cat and pineapples https://t.co/m7LvMabAM5')
-# print(f'Model Output: \n{out}\n')
-
